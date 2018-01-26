fb3cedfd4567e52e15cff4ce3c47c204e463c516
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-26 10:50:22 -0800

    added failed test case to access outstanding_principal on a given day of an account

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe Account, type: :model do
   describe "#deposit" do
 
   end
+
+  describe "#outstanding_principal" do
+    let(:now) { Time.now }
+    let(:account) {
+      a = Account.create(apr: 35.00, limit: 1000, created_at: now - 30.days)
+      a.withdraw!(500.00)
+      a.deposit!(200.00)
+      a.ledgers[0].created_at = now - 15.days
+      a.ledgers[1].created_at = now - 10.days
+      a.ledgers.each { |ledger| ledger.save }
+      a
+    }
+
+    it "should return outstanding principal for a given day" do
+      expect(a.outstanding_principal(t - 25.days)).to eq(0)
+    end
+  end
 end
