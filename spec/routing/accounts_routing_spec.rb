1872915b0bd1f1bdc5ffc4e313b380cff41b76b1
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-25 20:26:57 -0800

    commented few test cases which are not necessary at this time

--- spec/routing/accounts_routing_spec.rb
+++ spec/routing/accounts_routing_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe AccountsController, type: :routing do
   describe "routing" do
 
-    it "routes to #index" do
-      expect(:get => "/accounts").to route_to("accounts#index")
-    end
+    # it "routes to #index" do
+    #   expect(:get => "/accounts").to route_to("accounts#index")
+    # end
 
 
     it "routes to #show" do
@@ -17,16 +17,20 @@ RSpec.describe AccountsController, type: :routing do
       expect(:post => "/accounts").to route_to("accounts#create")
     end
 
-    it "routes to #update via PUT" do
-      expect(:put => "/accounts/1").to route_to("accounts#update", :id => "1")
-    end
+    # it "routes to #update via PUT" do
+    #   expect(:put => "/accounts/1").to route_to("accounts#update", :id => "1")
+    # end
 
-    it "routes to #update via PATCH" do
-      expect(:patch => "/accounts/1").to route_to("accounts#update", :id => "1")
-    end
+    # it "routes to #update via PATCH" do
+    #   expect(:patch => "/accounts/1").to route_to("accounts#update", :id => "1")
+    # end
 
-    it "routes to #destroy" do
-      expect(:delete => "/accounts/1").to route_to("accounts#destroy", :id => "1")
+    # it "routes to #destroy" do
+    #   expect(:delete => "/accounts/1").to route_to("accounts#destroy", :id => "1")
+    # end
+
+    it "routes to #withdraw" do
+      expect(:put => "/accounts/1/withdraw").to route_to("accounts#withdraw", :id => "1")
     end
 
   end
