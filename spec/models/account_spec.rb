43565371fc3a2af01787b11eebd3f9463f33dab4
Mohammad Khan <mohammad.khan@gmx.us>
2018-01-26 23:07:57 -0800

    added another test case for accumulated_interest for account which has multiple transactions during statement period

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -39,17 +39,35 @@ RSpec.describe Account, type: :model do
   end
 
   describe "#accumulated_interest" do
+    context "with one withdraw" do
       let(:now) { Time.now }
-      let(:account) {
-        a = Account.create(apr: 35.00, limit: 1000, created_at: now - 30.days)
-        a.withdraw!(500.00)
-        a.ledgers[0].created_at = now - 30.days
-        a.ledgers.each { |ledger| ledger.save }
-        a
-      }
+      let(:account) { Account.create(apr: 35.00, limit: 1000, created_at: now - 30.days) }
 
-    it "should calculate interest for each day for the outstanding principal of last 30 days" do
-      expect(account.accumulated_interest).to eq(14.38)
+      it "should calculate interest for each day for the outstanding principal of last 30 days" do
+        account.withdraw!(500.00)
+        account.ledgers[0].created_at = now - 30.days
+        account.ledgers.each { |ledger| ledger.save }
+
+        expect(account.accumulated_interest).to eq(14.38)
+      end
+    end
+
+    context "with multiple transactions" do
+      let(:now) { Time.now }
+      let(:account) { Account.create(apr: 35.00, limit: 1000, created_at: now - 30.days) }
+
+      it "should consider withdraw and deposit both to calculate interest" do
+        account.withdraw!(500.00)
+        account.deposit!(200.00)
+        account.withdraw!(100.00)
+        account.ledgers[0].created_at = now - 30.days
+        account.ledgers[1].created_at = now - 15.days
+        account.ledgers[2].created_at = now - 5.days
+
+        account.ledgers.each { |ledger| ledger.save }
+
+        expect(account.accumulated_interest).to eq(11.99)        
+      end
     end
   end
 end
